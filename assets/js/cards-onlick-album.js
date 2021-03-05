a8b3a9fe46fad3d89c0c519c424674cb0ac7fe2b
ardigermenji <ardi.germenji@gmail.com>
2021-03-05 18:02:36 +0100

    a

--- assets/js/cards-onlick-album.js
+++ assets/js/cards-onlick-album.js
@@ -1,22 +1,6 @@
 // jsEntryPoint.addEventListener('click', getCardInfo);
 
 function getCardsInfo(e) {
-  // switch (true) {
-  //   case !e.target.classList.contains('card-img-wrapper'):
-  //     console.log('none of your business, go click somewhere else! :P');
-
-  //   case !e.target.classList.contains('img-fluid'):
-  //     console.log('none of your business, go click somewhere else! :P');
-
-  //   case !e.target.classList.contains('card-title-custom'):
-  //     console.log('none of your business, go click somewhere else! :P');
-
-  //   case !e.target.classList.contains('card-text-custom'):
-  //     console.log('none of your business, go click somewhere else! :P');
-
-  //   case !e.target.classList.contains('card-body-custom'):
-  //     console.log('none of your business, go click somewhere else! :P');
-  // }
   console.log('hi');
   const card = e.target.closest('.card-custom');
   const title = card.querySelector('.card-title-custom').innerText;
